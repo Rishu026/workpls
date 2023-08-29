2ab94c017c7a96827b377ef3c74661730801950f
Rishabh <rishabh.garg2021@vitstudent.ac.in>
2023-08-29 14:40:28 +0530

    working on searchbar

---
+++ src/components/pages/objects/searchresultlist.js
@@ -0,0 +1,12 @@
+import "./searchresultlist.css";
+import { SearchResult } from "./searchlist";
+
+export const SearchResultsList = ({ results }) => {
+  return (
+    <div className="results-list">
+      {results.map((result, id) => {
+        return <SearchResult result={result.name} key={id} />;
+      })}
+    </div>
+  );
+};
