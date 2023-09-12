bf27349b17a02186a4e3f2857dcd110b87e4f07a
Rishabh <rishabh.garg2021@vitstudent.ac.in>
2023-09-12 19:48:42 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import Loginpage from "./components/pages/loginpage";
 
 function App() {
   return (
-    <div className="App ">
+    <div className="min-h-full min-w-full bg-amber-100 ">
       <Navbar />
+     
       <Routes>
         <Route path="/" element={<Maps/>} />
         <Route path = "/Alld" element = {<Alld/>}/>
--- src/components/pages/alldata.js
+++ src/components/pages/alldata.js
@@ -11,19 +11,20 @@ function Alld() {
   const [results, setResults] = useState([]);
 
   return (
-    <div className="Alld">
-        <div className="search-bar-container">
+    <div className='bg-cover flex-row bg-amber-100 bg-origin-border '>
+      
+        <div className="search-bar-container ">
         <SearchBar setResults={setResults}  /> 
         {results && results.length > 0 && <SearchResultsList results={results} />}   
         </div>  
 
-        <div className="map">
+        <div className="map ">
         <Mapcomponent results = {results} /> 
         </div>
-        
       <div className="chart-container">
       {results && results.length ===1 && <LineChart result ={results}/>}
       </div>
+     
     </div>
   );
 }
